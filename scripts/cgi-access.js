38ef8a6f477110c2e612df2862c66c35b8816017
LTSchmiddy <schmidalex1996@gmail.com>
2019-12-11 15:57:56 -0500

    added php check p2

--- scripts/cgi-access.js
+++ scripts/cgi-access.js
@@ -1,9 +1,27 @@
 "use strict";
 
 
-function checkForPHP() {
-    $.get("cgi-bin/php_version_info.php", {}, function(data, status) {
+
+function checkForPHP(successFunction, failFunction) {
+    console.log("Starting PHP Check...");
+    return $.get("cgi-bin/php_version_info.php", {}, function(data, status) {
+        if (status !== 'success'){
+            return false;
+        }
+
         console.log(data);
+
+        if (data.toString().startsWith("<!DOCTYPE html PUBLIC")) {
+            successFunction();
+        } else {
+            failFunction();
+        }
+
+
+    }).then(result => {
+        console.log("PHP Check Succeeded");
+    }, result => {
+        console.log('PHP Check Failed');
     });
 }
 
